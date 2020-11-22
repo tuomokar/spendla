72c076248acc8fa02499fe3f7a604099ab36a5a3
Tuomo Oila <tuomo.oila@gmail.com>
2020-11-22 23:41:25 +0200

    Auth API to be public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,13 @@ import { Controller, UseGuards, Post, Request } from '@nestjs/common';
 
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
